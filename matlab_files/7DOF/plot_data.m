50126cc12e237808566c0becf7d553562d1c19f8
Mark The Magnificent <marco.negrete@ingenieria.unam.edu>
2021-05-12 12:22:25 -0500

    Add version of article for conference and jist

--- matlab_files/7DOF/plot_data.m
+++ matlab_files/7DOF/plot_data.m
@@ -6,21 +6,23 @@ for i=1:7
   end
   figure
   hold on
-  plot(t, out.measured_q.signals.values(:,i), 'Color', [0.00,0.00,1.00], 'LineWidth',3)
-  plot(t, out.SMO_q.signals.values(:,i),      'Color', [0.25,0.75,0.95], 'LineWidth',3)
-  plot(t, desired_q,                          'Color', [1.00,0.00,0.00], 'LineWidth',3)
-  plot(t, out.EKF_q.signals.values(:,i)     , 'Color', [0.00,0.50,0.00], 'LineWidth',3)
-  xlabel('Time [s]', 'Fontsize', 15)
-  ylabel(strcat('Position $q_',int2str(i),'$ [rad]'), 'interpreter','latex', 'Fontsize',20)
-  legend({'Measured','SMO Estimated','Goal','EKF Filtered'}, 'Location','southeast', 'Fontsize',13)
+  plot(t, out.measured_q.signals.values(:,i), 'Color', [0.00,0.00,1.00], 'LineWidth',5)
+  plot(t, out.SMO_q.signals.values(:,i),      'Color', [0.25,0.75,0.95], 'LineWidth',5)
+  plot(t, desired_q,                          'Color', [1.00,0.00,0.00], 'LineWidth',5)
+  plot(t, out.EKF_q.signals.values(:,i)     , 'Color', [0.00,0.50,0.00], 'LineWidth',5)
+  set(gca,'FontSize',20)
+  %xlabel('Time [s]', 'Fontsize', 20)
+  %ylabel(strcat('Position $q_',int2str(i),'$ [rad]'), 'interpreter','latex', 'Fontsize',20)
+  %legend({'Measured','SMO Estimated','Goal','EKF Filtered'}, 'Location','southeast', 'Fontsize',13)
 
   figure
   hold on
-  plot(t, out.SMO_speeds.signals.values(:,i), 'Color', [0.25,0.75,0.95], 'LineWidth',3)
-  plot(t, out.real_qp.signals.values(:,i)   , 'Color', [0.00,0.00,1.00], 'LineWidth',3)
-  plot(t, desired_qp                        , 'Color', [0.00,0.50,0.00], 'LineWidth',3)
-  plot(t, out.EKF_speeds.signals.values(:,i), 'Color', [1.00,0.00,0.00], 'LineWidth',3)
-  xlabel('Time [s]', 'Fontsize', 15)
-  ylabel(strcat('Speed $\dot{q}_',int2str(i),'$ [rad/s]'), 'interpreter','latex', 'Fontsize', 20)
-  legend({'SMO Estimated','Real Speed','Goal','EKF Estimated'}, 'Fontsize',13)
+  plot(t, out.SMO_speeds.signals.values(:,i), 'Color', [0.25,0.75,0.95], 'LineWidth',5)
+  plot(t, out.real_qp.signals.values(:,i)   , 'Color', [0.00,0.00,1.00], 'LineWidth',5)
+  plot(t, desired_qp                        , 'Color', [0.00,0.50,0.00], 'LineWidth',5)
+  plot(t, out.EKF_speeds.signals.values(:,i), 'Color', [1.00,0.00,0.00], 'LineWidth',5)
+  set(gca,'FontSize',20)
+  %xlabel('Time [s]', 'Fontsize', 15)
+  %ylabel(strcat('Speed $\dot{q}_',int2str(i),'$ [rad/s]'), 'interpreter','latex', 'Fontsize', 20)
+  %legend({'SMO Estimated','Real Speed','Goal','EKF Estimated'}, 'Fontsize',13)
 end
